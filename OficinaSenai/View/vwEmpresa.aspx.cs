041a197c50e38a756c3a8e25b69f746dfb84519a
Ulisses Lino <ulissese.lino@outlook.com>
2018-09-24 14:32:07 -0300

    Coisas

--- OficinaSenai/View/vwEmpresa.aspx.cs
+++ OficinaSenai/View/vwEmpresa.aspx.cs
@@ -1,30 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ExemploBD.View
 {
-    public partial class vwEmpresa : System.Web.UI.Page
+    public partial class vwEmpresa : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void btnListar_Click(object sender, EventArgs e)
+        protected void btnListarOS_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(txtIdCliente.Text))
-            {
-                PopulateGrid(txtIdCliente.Text);
-            }
-        }
-
-        private void PopulateGrid(string idCliente)
-        {
-            
+            Response.Redirect("~/View/vwOrdemServico.aspx");
         }
     }
 }
